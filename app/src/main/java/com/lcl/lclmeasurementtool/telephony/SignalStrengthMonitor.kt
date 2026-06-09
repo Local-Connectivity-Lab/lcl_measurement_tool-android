d3c04c8de9e1688c1ab2adc5250b8fb7c6bf1c70
Ananya <68286155+ananyaa06@users.noreply.github.com>
2026-06-08 23:36:10 -0700

    feat: implement site-based cell ID matching (#16)
    * feat: use CellID from API instead of old implementation
    
    * fix: build error due to new dependency
    
    * fix: removing depracated package
    
    * refactor: change URL in NetworkConstants back from what it was during testing
    
    * refactor: add comment for calculateDistance function
    
    Added a comment to explain the purpose of the calculateDistance function.

--- app/src/main/java/com/lcl/lclmeasurementtool/telephony/SignalStrengthMonitor.kt
+++ app/src/main/java/com/lcl/lclmeasurementtool/telephony/SignalStrengthMonitor.kt
@@ -1,9 +1,11 @@
 package com.lcl.lclmeasurementtool.telephony
 
 import android.telephony.SignalStrength
+import com.lcl.lclmeasurementtool.model.datamodel.Site
 import kotlinx.coroutines.flow.Flow
 
 interface SignalStrengthMonitor {
     val signalStrength: Flow<SignalStrength>
     fun getCellID(): String
+    fun getCellIDFromSite(latitude: Double, longitude: Double, sites: List<Site>): String?
 }
